73c87225c5766dadee5b8e5304ca68736a960d05
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-08-01 21:53:31 +0100

    Initial commit

---
+++ src/main/java/com/example/javakatatwo/JavaKataTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javakatatwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKataTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKataTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javakatatwo/JavaKataTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javakatatwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKataTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
