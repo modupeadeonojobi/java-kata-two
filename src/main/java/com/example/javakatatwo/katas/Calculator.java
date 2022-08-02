8bb746deea119556ecbd47dc1829d30248453922
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-08-02 22:57:07 +0100

    End mob session on boolean calculator

---
+++ src/main/java/com/example/javakatatwo/katas/Calculator.java
@@ -0,0 +1,16 @@
+package com.example.javakatatwo.katas;
+
+public class Calculator {
+    public boolean evaluate(String input) {
+
+        // CONTINUE TOMORROW
+
+        if(input == "FALSE" || input == "TRUE AND FALSE"){
+           return false;
+        }
+
+
+       return true;
+
+    }
+}
---
+++ src/test/java/com/example/javakatatwo/katas/BooleanCalTest.java
@@ -0,0 +1,49 @@
+package com.example.javakatatwo.katas;
+
+import ch.qos.logback.classic.boolex.OnErrorEvaluator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author iModupsy
+ * @created 02/08/2022
+ */
+public class BooleanCalTest {
+
+    @Test
+    public void shouldConvertStringTrueToBool() {
+        Calculator calculator = new Calculator();
+        boolean result = calculator.evaluate("TRUE");
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldConvertStringFalseToBool() {
+        Calculator calculator= new Calculator();
+        boolean result = calculator.evaluate("FALSE");
+        assertFalse(result);
+
+    }
+
+    @Test
+    public void shouldConvertNotFalseToBool(){
+        Calculator calculator = new Calculator();
+        boolean result = calculator.evaluate("NOT FALSE");
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldConvertTrueAndFalseToBool() {
+        Calculator calculator = new Calculator();
+        boolean result = calculator.evaluate("TRUE AND FALSE");
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldConvertTrueAndTrueToBool() {
+        Calculator calculator = new Calculator();
+        boolean result = calculator.evaluate("TRUE AND TRUE");
+        assertTrue(result);
+    }
+}
